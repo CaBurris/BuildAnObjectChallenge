2fd3173ec291484210a11863a75004da2f656f2a
Christy <caburris@ucdavis.edu>
2018-03-15 12:28:34 -0700

    Completed challenge

--- js/student_report.js
+++ js/student_report.js
@@ -1,17 +1,37 @@
 var message = ' ';
 var student;
+var search;
+var found = false;
 
 function print(message) {
     var outputDiv = document.getElementById('output');
     outputDiv.innerHTML = message;
 }
 
-for ( var i = 0; i < students.length; i += 1){
-    student = students[i];
-    message += '<h2>Student: ' + student.name + '</h2>';
-    message += '<p>Track: ' + student.track + '</p>';
-    message += '<p>Points: ' + student.points + '</p>';
-    message += '<p>Achievements: ' + student.achievements + '</p>';
-
+function getStudentReport ( student ){
+    var report = '<h2>Student: ' + student.name + '</h2>';
+    report += '<p>Track: ' + student.track + '</p>';
+    report += '<p>Points: ' + student.points + '</p>';
+    report += '<p>Achievements: ' + student.achievements + '</p>';
+    return report;
 }
-print(message);
+
+while (true) {
+    var search = prompt('Search student records: type a name [Christy, Dave, Jen, Louis, Marie] followed by "quit" to view data (or type "quit" to end)');
+    if (search === null || search.toLowerCase() === 'quit') {
+        break;
+    } else {
+        for ( var i = 0; i < students.length; i += 1){
+            student = students[i];
+            if (student.name === search) {
+                message += getStudentReport (student);
+                found = true;
+            }
+        }
+        if (found == false) {
+            print ('No student named ' + search + ' is found.')
+        }else {
+            print(message);
+        }
+    }
+}
--- js/students.js
+++ js/students.js
@@ -1,7 +1,7 @@
 var students = [
-    {name: 'Christy ', track: 'Front End Development ', achievements: 33 , points: 2936 },
-    {name: 'Dave ', track: 'Front End Development ', achievements: 158, points: 14730},
-    {name: 'Jen ', track: 'Front End Development ', achievements: 10, points: 50},
-    {name: 'Louis ', track: 'Front End Development ', achievements: 80, points: 300},
+    {name: 'Christy', track: 'Front End Development ', achievements: 33 , points: 2936 },
+    {name: 'Dave', track: 'Front End Development ', achievements: 158, points: 14730},
+    {name: 'Jen', track: 'Front End Development ', achievements: 10, points: 50},
+    {name: 'Louis', track: 'Front End Development ', achievements: 80, points: 300},
     {name: 'Marie', track: 'Front End Development ', achievements: 130 , points: 1000}
 ];
